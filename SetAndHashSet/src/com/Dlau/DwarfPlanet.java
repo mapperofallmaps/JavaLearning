2079f6dcdbe486dca8b8c8edf818ad42ce33a237
mapperofallmaps <35754554+mapperofallmaps@users.noreply.github.com>
2018-03-07 01:26:23 +1100

    Sets

---
+++ SetAndHashSet/src/com/Dlau/DwarfPlanet.java
@@ -0,0 +1,8 @@
+package com.Dlau;
+
+public class DwarfPlanet extends HeavenlyBody {
+
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+}
